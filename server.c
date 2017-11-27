8d8201318bc09169217c6ecfca39f7d29a85969e
tarnacious <tarn@tarnbarford.net>
2017-11-27 14:06:29 +0100

    Very basic HTTPS proxy tunneling

---
+++ server.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <string.h>
+#include <strings.h>
+
+#define BUFFER_SIZE 1024
+
+int create_socket(int port)
+{
+    int s;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+	perror("Unable to create socket");
+	exit(EXIT_FAILURE);
+    }
+
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+	perror("Unable to bind");
+	exit(EXIT_FAILURE);
+    }
+
+    if (listen(s, 1) < 0) {
+	perror("Unable to listen");
+	exit(EXIT_FAILURE);
+    }
+
+    return s;
+}
+
+void init_openssl()
+{
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_openssl()
+{
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+	perror("Unable to create SSL context");
+	ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx)
+{
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate_file(ctx, "device.crt", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "device.key", SSL_FILETYPE_PEM) <= 0 ) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int sock;
+    SSL_CTX *ctx;
+
+    init_openssl();
+    ctx = create_context();
+
+    configure_context(ctx);
+
+    sock = create_socket(4433);
+
+    /* Handle connections */
+    while(1) {
+        struct sockaddr_in addr;
+        unsigned int len = sizeof(addr);
+        SSL *ssl;
+        char buffer[BUFFER_SIZE];
+        int bytes_read;
+
+        int client = accept(sock, (struct sockaddr*)&addr, &len);
+        if (client < 0) {
+            perror("Unable to accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // lets just assume, we can read the whole thing and it fits in the buffer :)
+        bzero(buffer, BUFFER_SIZE);
+        fprintf(stderr, "Reading header\n");
+        read(client, buffer, BUFFER_SIZE - 1);
+        fprintf(stderr, "%s", buffer);
+        bzero(buffer, BUFFER_SIZE);
+
+        // lets just assume it was a HTTP/1.1 request and write a response
+        const char message[] = "HTTP/1.1 200 OK\r\n\r\n";
+        write(client, message, strlen(message));
+
+        ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client);
+
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+        }
+        else {
+            bzero(buffer, BUFFER_SIZE);
+            bytes_read = SSL_read(ssl, buffer, 100);
+            fprintf(stderr, "bytes read: %d\r\n\r\n", bytes_read);
+            fprintf(stderr, "%s\r\n\r\n", buffer);
+            bzero(buffer, BUFFER_SIZE);
+
+            const char reply[] = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\n\r\n";
+            fprintf(stderr, "writing response:\r\n%s", reply);
+            SSL_write(ssl, reply, strlen(reply));
+        }
+
+        SSL_free(ssl);
+        close(client);
+    }
+
+    close(sock);
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
+}
